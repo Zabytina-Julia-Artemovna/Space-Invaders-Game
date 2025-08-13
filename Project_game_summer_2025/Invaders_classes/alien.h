6f0b3a9e3662497033938f9ff1359301e5ace93c
Юля Забытина <user@mail.ru>
2025-08-13 14:08:00 +0300

    Исправлен класс Alien (по ошибке был добавлен код другого класса)

--- Project_game_summer_2025/Invaders_classes/alien.h
+++ Project_game_summer_2025/Invaders_classes/alien.h
@@ -1,20 +1,46 @@
 #pragma once
 #include "game_object.h"
 namespace Invaders {
-    class Player : public GameObject {
+    enum class AlienType {
+        SMALL,
+        MEDIUM,
+        LARGE
+    };
+    class Alien : public GameObject {
     private:
-        char _player_appearance = '^';
+        AlienType _type;
+        char _alien_appearance;
+        size_t _cost;
     public:
-        Player(int x, int y) : GameObject(x, y) {}
-        char getAppearance() const {
-            return _player_appearance;
+        Alien(int x, int y, AlienType type) :
+            GameObject(x, y), _type(type) {
+            switch (_type) {
+            case AlienType::SMALL:
+                _alien_appearance = 's';
+                _cost = 10;
+                break;
+            case AlienType::MEDIUM:
+                _alien_appearance = 'm';
+                _cost = 20;
+                break;
+            case AlienType::LARGE:
+                _alien_appearance = 'l';
+                _cost = 30;
+                break;
+            }
         }
-        void Shoot(); // возвращает void или Bullet* ?
-        void MoveLeft();
-        void MoveRight();
+        AlienType getType() const {
+            return _type;
+        }
+        char getAppearance() const {
+            return _alien_appearance;
+        }
+        size_t getCost() const {
+            return _cost;
+        }
+        void Shoot();
         //void OnCollision(GameObject* other) override;
-        //void DrawOn() const override;
+        //void Draw() const override;
         void Update() override;
     };
 }
-
