007e88c3639995d2520bd4adba28ca8099fa1d4a
Юля Забытина <user@mail.ru>
2025-08-13 17:01:15 +0300

    Добавлены функции отрисовки объекта в консоли (класс GameObject)

--- Project_game_summer_2025/Invaders_classes/game_object.h
+++ Project_game_summer_2025/Invaders_classes/game_object.h
@@ -1,41 +1,38 @@
 #pragma once
+#include <windows.h>
 namespace Invaders {
-    struct Coordinates;
-    class IDrawable;
-    class IDisplay;
-
+    struct Coordinates {
+        int x;
+        int y;
+    };
     struct IDisplay {
         virtual void print(int x, int y, char c) = 0;
         virtual ~IDisplay() = default;
     };
-
-    class ConsoleDisplay : public IDisplay {
-    public:
-        void print(int x, int y, char c) override {
-            // Реализация метода
-        }
-    };
-
     struct IDrawable {
         virtual void DrawOn(IDisplay& out) const = 0;
         virtual void Update() = 0;
         virtual ~IDrawable() = default;
     };
-
     enum Event {
         KeyPress = 0
     };
-
-    struct Coordinates {
-        int x;
-        int y;
+    class ConsoleDisplay : public IDisplay {
+    public:
+        void print(int x, int y, char c) override {
+            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+            COORD coordinates = { 
+                static_cast<SHORT>(x - 1), static_cast<SHORT>(y - 1) 
+            };
+            SetConsoleCursorPosition(hConsole, coordinates);
+            WriteConsoleOutputCharacterA(hConsole, &c, 1, coordinates, nullptr);
+        }
     };
-
     class GameObject : public IDrawable {
     protected:
         Coordinates _position;
         bool _is_active;
-
+        char _appearance;
     public:
         GameObject(int x, int y) :
             _position{ x, y },
@@ -43,10 +40,7 @@ namespace Invaders {
 
         virtual void OnEvent(Event e, char c) = 0;
         void DrawOn(IDisplay& out) const override {
-            // Реализация метода
-        }
-        void Update() override {
-            // Реализация метода
+            out.print(_position.x, _position.y, _appearance);
         }
     };
 }
