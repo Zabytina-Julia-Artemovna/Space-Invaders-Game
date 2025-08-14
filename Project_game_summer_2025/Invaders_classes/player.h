563df849fa64a989bd332b105055856172fba9f4
Юля Забытина <user@mail.ru>
2025-08-14 10:56:42 +0300

    Доработана функция OnEvent для класса Player

--- Project_game_summer_2025/Invaders_classes/player.h
+++ Project_game_summer_2025/Invaders_classes/player.h
@@ -1,19 +1,23 @@
 #pragma once
 #include "game_object.h"
+#include <windows.h>
 namespace Invaders {
     class Player : public GameObject {
-    private:
-        char _player_appearance = '^';
     public:
-        Player(int x, int y) : GameObject(x, y) {}
-        char getAppearance() const {
-            return _player_appearance;
+        Player(int x, int y) : GameObject(x, y) {
+            _appearance = 'W';
         }
-        void Shoot(); // возвращает void или Bullet* ?
-        void MoveLeft();
-        void MoveRight();
+        void OnEvent(Event e, char c) override {
+            int newX =  _position.x;
+            int newY =  _position.y;
+            if ( (c == 'D' || c == 'd') && newX+1 < 208) {
+                newX++;
+            }
+            else if ( (c == 'A' || c == 'a') && newX-1 > 0) {
+                newX--;
+            }
+            UpdatePosition(newX, newY);
+        }
         //void OnCollision(GameObject* other) override;
-        //void DrawOn() const override;
-        void Update() override;
     };
 }
