7d6b364aa0754b904f62ea8cad9311006187de7b
Юля Забытина <user@mail.ru>
2025-08-14 10:46:24 +0300

    Добавлен метод UpdatePosition в класс GameObject - для ообновления позиции объекта при движении, добавлены методы очистки экрана

--- Project_game_summer_2025/Invaders_classes/game_object.h
+++ Project_game_summer_2025/Invaders_classes/game_object.h
@@ -7,10 +7,12 @@ namespace Invaders {
     };
     struct IDisplay {
         virtual void print(int x, int y, char c) = 0;
+        virtual void clear(int x, int y) = 0;
         virtual ~IDisplay() = default;
     };
     struct IDrawable {
         virtual void DrawOn(IDisplay& out) const = 0;
+        virtual void ClearOn(IDisplay& out) const = 0;
         virtual ~IDrawable() = default;
     };
     enum class Event {
@@ -29,20 +31,40 @@ namespace Invaders {
             DWORD written;
             WriteConsoleOutputCharacterA(hConsole, &c, 1, coordinates, &written);
         }
+        void clear(int x, int y) override {
+            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+            if (hConsole == INVALID_HANDLE_VALUE) return;
+            COORD coordinates = {
+                static_cast<SHORT>(x), static_cast<SHORT>(y)
+            };
+            SetConsoleCursorPosition(hConsole, coordinates);
+            DWORD written;
+            char c = ' ';
+            WriteConsoleOutputCharacterA(hConsole, &c, 1, coordinates, &written);
+        }
     };
     class GameObject : public IDrawable {
     protected:
         Coordinates _position;
+        Coordinates _previous_position;
         bool _is_active;
         char _appearance;
     public:
         GameObject(int x, int y) :
             _position{ x, y },
+            _previous_position{ x, y},
             _is_active(true) {}
 
         virtual void OnEvent(Event e, char c) = 0;
         void DrawOn(IDisplay& out) const override {
             out.print(_position.x, _position.y, _appearance);
+        }
+        void ClearOn(IDisplay& out) const override {
+            out.clear(_previous_position.x, _previous_position.y);
+        }
+        void UpdatePosition(int newX, int newY) {
+            _previous_position = _position;  
+            _position = { newX, newY };  
         }
     };
 }
