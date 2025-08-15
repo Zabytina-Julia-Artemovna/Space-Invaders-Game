a259b0c23468c795acc71437a86824b15c791133
Юля Забытина <user@mail.ru>
2025-08-15 19:11:46 +0300

    Доработана логика передвижения пришельцев

--- Project_game_summer_2025/Invaders_classes/alien.h
+++ Project_game_summer_2025/Invaders_classes/alien.h
@@ -2,7 +2,6 @@
 #include <chrono>
 #include "game_object.h"
 namespace Invaders {
-    std::chrono::steady_clock::time_point lastTime;
     class Alien : public GameObject {
     protected:
         size_t _cost;
@@ -13,38 +12,40 @@ namespace Invaders {
     public:
         virtual ~Alien() = default;
         Alien(int x, int y, char appearance, size_t cost,
-             int right_border, int left_border)
+            int right_border, int left_border)
             : GameObject(x, y, appearance),
             _cost(cost), _right_border(right_border),
             _left_border(left_border),
-            lastTime(std::chrono::steady_clock::now()){}
-
-        void OnEvent(Event e, char c) override
-        {
+            lastTime(std::chrono::steady_clock::now()) {}
+        void OnEvent(Event e, char c) override {
             auto currentTime = std::chrono::steady_clock::now();
             int durationTime = std::chrono::duration_cast<std::chrono::milliseconds>
                 (currentTime - lastTime).count();
+            durationTime = std::min<int>(durationTime, 100);
             lastTime = currentTime;
-            int distance = (_speed * durationTime) / 32;
+            int distance = (_speed * durationTime) / 64;
+            if (distance == 0 && durationTime > 0) {
+                distance = _speed > 0 ? 1 : -1;
+            }
             int newX = _position.x;
             int newY = _position.y;
             if (_speed > 0) {
                 if (newX + distance < _right_border) {
                     newX += distance;
-                } else {
-                    newY = _position.y + 1;
-                    _speed *= -1;
-                    newX = _right_border - 1;
-                }
-            }
-            else if (_speed < 0) {
-                if (newX + distance > _left_border) {
-                    newX += distance;
                 }
                 else {
                     newY = _position.y + 1;
                     _speed *= -1;
-                    newX = _left_border+1;
+                    newX = _right_border - 1;  
+                }
+            } else {
+                if (newX + distance > _left_border) {
+                    newX += distance;  
+                }
+                else {
+                    newY = _position.y + 1;
+                    _speed *= -1;
+                    newX = _left_border + 1; 
                 }
             }
             UpdatePosition(newX, newY);
