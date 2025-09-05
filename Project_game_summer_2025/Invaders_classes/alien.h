d28fb092efea20bf253a29f2d9df2fa7c3618cda
Юля Забытина <user@mail.ru>
2025-09-05 09:01:13 +0300

    Добавлена логика стрельбы для пришельца (при запуске работает, однако возникает RunTime Error: проблема с использовнием std::vector

--- Project_game_summer_2025/Invaders_classes/alien.h
+++ Project_game_summer_2025/Invaders_classes/alien.h
@@ -9,6 +9,12 @@ namespace Invaders {
         int _right_border;
         int _left_border;
         std::chrono::steady_clock::time_point lastTime;
+
+        std::chrono::steady_clock::time_point _lastShotTime;
+        std::vector<Bullet> _invaders_bullets;
+        bool _canShoot = false;
+        const int _shootCooldown = 3000;
+
     public:
         virtual ~Alien() = default;
         Alien(int x, int y, char appearance, size_t cost,
@@ -18,38 +24,67 @@ namespace Invaders {
             _left_border(left_border),
             lastTime(std::chrono::steady_clock::now()) {}
         void OnEvent(Event e, char c) override {
-            auto currentTime = std::chrono::steady_clock::now();
-            int durationTime = std::chrono::duration_cast<std::chrono::milliseconds>
-                (currentTime - lastTime).count();
-            durationTime = std::min<int>(durationTime, 100);
-            lastTime = currentTime;
-            int distance = (_speed * durationTime) / 64;
-            if (distance == 0 && durationTime > 0) {
-                distance = _speed > 0 ? 1 : -1;
-            }
-            int newX = _position.x;
-            int newY = _position.y;
-            if (_speed > 0) {
-                if (newX + distance < _right_border) {
-                    newX += distance;
+            if (e == Event::Timer) {
+
+                auto currentTime = std::chrono::steady_clock::now();
+                int durationTime = std::chrono::duration_cast<std::chrono::milliseconds>
+                    (currentTime - lastTime).count();
+                durationTime = std::min<int>(durationTime, 100);
+                lastTime = currentTime;
+                int distance = (_speed * durationTime) / 64;
+                if (distance == 0 && durationTime > 0) {
+                    distance = _speed > 0 ? 1 : -1;
+                }
+                int newX = _position.x;
+                int newY = _position.y;
+                if (_speed > 0) {
+                    if (newX + distance < _right_border) {
+                        newX += distance;
+                    }
+                    else {
+                        newY = _position.y + 1;
+                        _speed *= -1;
+                        newX = _right_border - 1;
+                    }
                 }
                 else {
-                    newY = _position.y + 1;
-                    _speed *= -1;
-                    newX = _right_border - 1;  
+                    if (newX + distance > _left_border) {
+                        newX += distance;
+                    }
+                    else {
+                        newY = _position.y + 1;
+                        _speed *= -1;
+                        newX = _left_border + 1;
+                    }
                 }
-            } else {
-                if (newX + distance > _left_border) {
-                    newX += distance;  
-                }
-                else {
-                    newY = _position.y + 1;
-                    _speed *= -1;
-                    newX = _left_border + 1; 
-                }
+                UpdatePosition(newX, newY);
+                TryShoot(_position.x, _position.y);
+
             }
-            UpdatePosition(newX, newY);
         }
-        virtual void Shoot() = 0;
+        void TryShoot(int x, int y) { 
+            if (_is_active) {
+                auto currentTime = std::chrono::steady_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>
+                    (currentTime - _lastShotTime).count();
+                if (duration > _shootCooldown) {
+                    _canShoot = true;
+                }
+                if (_canShoot) {
+                    Bullet bullet(x, y + 1, false);
+                    _invaders_bullets.push_back(bullet);
+                    _canShoot = false;
+                    _lastShotTime = currentTime;
+                }
+            }
+
+        }
+        size_t getBulletsCount() const {
+            return _invaders_bullets.size();
+        }
+        std::vector<Bullet>& getBullets() {
+            return _invaders_bullets;
+        }
+        
     };
 }
