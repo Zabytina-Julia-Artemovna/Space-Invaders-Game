49a7eb6f26b3f12b8f608ce4205c4a9b6a8cc201
Юля Забытина <user@mail.ru>
2025-09-10 16:01:56 +0300

    Добавлен оператор присваивания

--- Project_game_summer_2025/Invaders_classes/white_alien.h
+++ Project_game_summer_2025/Invaders_classes/white_alien.h
@@ -5,10 +5,24 @@ namespace Invaders {
     public:
         WhiteAlien(int x, int y, int right_border,
             int left_border) :
-            Alien(x, y, 'G', 10, right_border, left_border) {}
-        void Shoot() override {
+            Alien(x, y, 'W', 50, right_border, left_border) {}
+        WhiteAlien& operator=(const WhiteAlien& other) {
+            if (this != &other) {
+                _position = other._position;
+                _is_active = other._is_active;
+                _cost = other._cost;
+                _speed = other._speed;
+                _right_border = other._right_border;
+                _left_border = other._left_border;
+                _lastTime = other._lastTime;
+                _lastShotTime = other._lastShotTime;
+                _canShoot = other._canShoot;
 
+                _invaders_bullets = other._invaders_bullets;
+            }
+            return *this;
         }
 
+        
     };
 }
