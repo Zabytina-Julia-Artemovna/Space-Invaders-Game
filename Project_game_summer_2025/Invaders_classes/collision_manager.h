738c7f4a996408084ba6f9d06a25c8bf5432931b
Юля Забытина <user@mail.ru>
2025-09-10 14:42:58 +0300

    Добавлены методы для проверки столкновений объектов (игрок - пришелец, игрок - снаряд пришельца, пришелец - снаряд игрока)

--- Project_game_summer_2025/Invaders_classes/collision_manager.h
+++ Project_game_summer_2025/Invaders_classes/collision_manager.h
@@ -14,26 +14,112 @@ namespace Invaders {
             static CollisionManager instance;
             return instance;
         }
-        bool checkCollisions(Player& player, Alien& alien) {
-            if (checkPlayerVsAlien()) {
+        bool checkCollisions(Player& player, AlienManager& aliens) {
+            if (checkPlayerVsAliens(player, aliens)) {
                 return true;
             }
-            if (checkPlayerVsBullet()) {
+            if (checkPlayerVsAlienBullet(player, aliens)) {
                 return true;
             }
-            if (checkAlienVsBullet()) {
+            if (checkAlienVsPlayerBullet(player, aliens)) {
                 return true;
             }
             return false;
         }
-        bool checkPlayerVsAlien() {
-
+    private:
+        bool checkPlayerVsAliens(Player& player, AlienManager& aliens) {
+            for (size_t i = 0; i < aliens.getRedAliens().size(); ++i) {
+                if (player.getPosition().x == aliens.getRedAliens()[i].getPosition().x
+                    && player.getPosition().y == aliens.getRedAliens()[i].getPosition().y
+                    && aliens.getRedAliens()[i].isActive()) {
+                    return true;
+                }
+            }
+            for (size_t i = 0; i < aliens.getGreenAliens().size(); ++i) {
+                if (player.getPosition().x == aliens.getGreenAliens()[i].getPosition().x
+                    && player.getPosition().y == aliens.getGreenAliens()[i].getPosition().y
+                    && aliens.getGreenAliens()[i].isActive()) {
+                    return true;
+                }
+            }
+            for (size_t i = 0; i < aliens.getWhiteAliens().size(); ++i) {
+                if (player.getPosition().x == aliens.getWhiteAliens()[i].getPosition().x
+                    && player.getPosition().y == aliens.getWhiteAliens()[i].getPosition().y
+                    && aliens.getWhiteAliens()[i].isActive()) {
+                    return true;
+                }
+            }
+            return false;
         }
-        bool checkPlayerVsBullet() {
+        bool checkPlayerVsAlienBullet(Player& player, AlienManager& aliens) {
+            for (size_t i = 0; i < aliens.getRedAliens().size(); ++i) {
+                for (size_t j = 0; j < aliens.getRedAliens()[i].getBulletsCount(); ++j) {
+                    if (player.getPosition().x == aliens.getRedAliens()[i].getBullets()[j].getPosition().x
+                        && player.getPosition().y == aliens.getRedAliens()[i].getBullets()[j].getPosition().y
+                        && aliens.getRedAliens()[i].isActive()
+                        && aliens.getRedAliens()[i].getBullets()[j].isActive()) {
+                        return true;
+                    }
+                }
+            }
+            for (size_t i = 0; i < aliens.getGreenAliens().size(); ++i) {
+                for (size_t j = 0; j < aliens.getGreenAliens()[i].getBulletsCount(); ++j) {
+                    if (player.getPosition().x == aliens.getGreenAliens()[i].getBullets()[j].getPosition().x
+                        && player.getPosition().y == aliens.getGreenAliens()[i].getBullets()[j].getPosition().y
+                        && aliens.getGreenAliens()[i].isActive()
+                        && aliens.getGreenAliens()[i].getBullets()[j].isActive()) {
+                        return true;
+                    }
+                }
 
+            }
+            for (size_t i = 0; i < aliens.getWhiteAliens().size(); ++i) {
+                for (size_t j = 0; j < aliens.getWhiteAliens()[i].getBulletsCount(); ++j) {
+                    if (player.getPosition().x == aliens.getWhiteAliens()[i].getBullets()[j].getPosition().x
+                        && player.getPosition().y == aliens.getWhiteAliens()[i].getBullets()[j].getPosition().y
+                        && aliens.getWhiteAliens()[i].isActive()
+                        && aliens.getWhiteAliens()[i].getBullets()[j].isActive()) {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
-        bool checkAlienVsBullet() {
-
+        bool checkAlienVsPlayerBullet(Player& player, AlienManager& aliens) {
+            for (size_t i = 0; i < aliens.getRedAliens().size(); ++i) {
+                for (size_t j = 0; j < player.getBulletsCount(); ++j) {
+                    if (aliens.getRedAliens()[i].getPosition().x == player.getBullets()[j].getPosition().x
+                        && aliens.getRedAliens()[i].getPosition().y == player.getBullets()[j].getPosition().y
+                        && aliens.getRedAliens()[i].isActive()
+                        && player.getBullets()[j].isActive()) {
+                        aliens.getRedAliens()[i].setIsActive(false);
+                        return true;
+                    }
+                }
+            }
+            for (size_t i = 0; i < aliens.getGreenAliens().size(); ++i) {
+                for (size_t j = 0; j < player.getBulletsCount(); ++j) {
+                    if (aliens.getGreenAliens()[i].getPosition().x == player.getBullets()[j].getPosition().x
+                        && aliens.getGreenAliens()[i].getPosition().y == player.getBullets()[j].getPosition().y
+                        && aliens.getGreenAliens()[i].isActive()
+                        && player.getBullets()[j].isActive()) {
+                        aliens.getGreenAliens()[i].setIsActive(false);
+                        return true;
+                    }
+                }
+            }
+            for (size_t i = 0; i < aliens.getWhiteAliens().size(); ++i) {
+                for (size_t j = 0; j < player.getBulletsCount(); ++j) {
+                    if (aliens.getWhiteAliens()[i].getPosition().x == player.getBullets()[j].getPosition().x
+                        && aliens.getWhiteAliens()[i].getPosition().y == player.getBullets()[j].getPosition().y
+                        && aliens.getWhiteAliens()[i].isActive()
+                        && player.getBullets()[j].isActive()) {
+                        aliens.getWhiteAliens()[i].setIsActive(false);
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     };
 }
