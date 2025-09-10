3952b36ab28978d232f4bdee4d7181a0f3cc3dd5
Юля Забытина <user@mail.ru>
2025-09-10 16:00:12 +0300

    Добавлен оператор присваивания

--- Project_game_summer_2025/Invaders_classes/green_alien.h
+++ Project_game_summer_2025/Invaders_classes/green_alien.h
@@ -5,10 +5,9 @@ namespace Invaders {
     public:
         GreenAlien(int x, int y, int right_border,
             int left_border) :
-            Alien(x, y, 'G', 10, right_border, left_border) {}
-        void Shoot() override {
+            Alien(x, y, 'G', 25, right_border, left_border) {}
+       
 
-        }
-
+        
     };
 }
